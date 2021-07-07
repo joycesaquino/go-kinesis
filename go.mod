ca3f44a8ad7d4defd7f8d8c20f9a273a9b6dd2f8
joyce.aquino <joyce.aquino@b2wdigital.com>
2021-07-06 23:31:51 -0300

    [new] Implementando construção do cliente do kinesis

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/aws/aws-sdk-go-v2 v1.7.0
 	github.com/aws/aws-sdk-go-v2/service/kinesis v1.5.0
+	github.com/caarlos0/env/v6 v6.6.2
 )
--- internal/kinesis_client.go
+++ internal/kinesis_client.go
@@ -2,7 +2,10 @@ package internal
 
 import (
 	"context"
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/kinesis"
+	"github.com/caarlos0/env/v6"
+	"log"
 )
 
 type Config struct {
@@ -17,4 +20,18 @@ type KdsApi interface {
 type Client struct {
 	config     Config
 	awsKinesis KdsApi
+}
+
+func NewClient() *Client {
+	var config Config
+
+	err := env.Parse(&config)
+	if err != nil {
+		log.Fatal("[ERROR] - Couldn't build settings for kinesis stream client", err)
+	}
+
+	return &Client{
+		config:     config,
+		awsKinesis: kinesis.NewFromConfig(aws.Config{Region: config.Region}),
+	}
 }
