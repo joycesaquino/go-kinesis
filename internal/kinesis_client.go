406940cbab59dc5ccbcee7c40596ad0709da90d4
joyce.aquino <joyce.aquino@b2wdigital.com>
2021-07-07 00:27:33 -0300

    [new] Send com API do kds para facilitar no uso de interface para o teste

--- internal/kinesis_client.go
+++ internal/kinesis_client.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"context"
+	"encoding/json"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/kinesis"
 	"github.com/caarlos0/env/v6"
@@ -20,6 +21,23 @@ type KdsApi interface {
 type Client struct {
 	config     Config
 	awsKinesis KdsApi
+}
+
+func (c Client) SendWithApi(ctx context.Context, api KdsApi, data interface{}) error {
+	bytes, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if _, err = api.PutRecord(ctx, &kinesis.PutRecordInput{
+		Data: bytes,
+		// To use considering ordering if necessary PartitionKey: aws.String(data.Id),
+		StreamName: aws.String(c.config.StreamName),
+	}); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func NewClient() *Client {
